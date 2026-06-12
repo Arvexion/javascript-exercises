ed907fae5f02a22c026f6ba3f7edb12e8bc41620
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-12 10:24:24 +0800

    Add function that remove values from an array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+// Function that takes an array and returns an array without the seconds arguments
+const removeFromArray = function(array, ...args) {
+    // Creates a new array
+    const cleanArray = [];
+
+    // Goes through each item in the array
+    array.forEach((item) => {
+        // If the array has the item, add it to the new array
+        if (!args.includes(item)) {
+            cleanArray.push(item);
+        }
+    });
+
+    return cleanArray;
 };
 
 // Do not edit below this line
