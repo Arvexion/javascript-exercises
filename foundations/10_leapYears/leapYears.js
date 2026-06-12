9fdf800cb874eb5cbe12bdc5cd3ebbfe688bdc47
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-12 11:11:51 +0800

    Add function that checks if a year is a leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+// Function that returns a true if the argument is a leap year and false if not
+const leapYears = function(year) {
+    // Checks if year can be divided by four
+    if ((year) % 4 === 0) {
+        // Checks if year can be divided by 100
+        if ((year) % 100 === 0) {
+            // If year can be divided by 100, check if it can be divided by 400
+            if ((year) % 400 === 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
+        // returns year
 };
 
 // Do not edit below this line
