60890037f77b5dbfa8e8e2de5812680b75647233
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-12 11:37:05 +0800

    Add function that converts temperatures

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = 0;
+
+  // Converts temp to Celsius using a formula
+  celsius = ((temp - 32) * 5 / 9)
+
+  // Rounds off the Celsius
+  celsius = Math.round(celsius * 10) / 10; 
+
+  // Returns the temp
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = 0;
+
+  // Converts temp to Fahrenheit using a formula
+  fahrenheit = (temp * 9 / 5 + 32);
+
+  // Rounds off the Fahrenheit
+  fahrenheit = Math.round(fahrenheit * 10) / 10; 
+
+  // Returns the temp
+  return fahrenheit;
 };
 
 // Do not edit below this line
