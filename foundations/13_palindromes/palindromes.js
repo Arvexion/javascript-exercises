19d6bd95a42646348ab46067c3712cda0c832311
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-17 15:53:58 +0800

    Add palindrome checker function

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const originalWord = word.toLowerCase().replace(/[^a-z0-9]/gi, "");
+    const reversedWord = originalWord.split('').reverse().join('');
 
+    return originalWord === reversedWord;
 };
 
 // Do not edit below this line
