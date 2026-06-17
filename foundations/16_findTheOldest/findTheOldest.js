e4fcba7780f3885ac0a3e81a145d8401a7541e2c
Arvexion <288262116+Arvexion@users.noreply.github.com>
2026-06-17 18:15:55 +0800

    Add function to get the oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
 
+    return people.reduce((prev, current) => {
+        const prevAge = getAge(prev.yearOfBirth, prev.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return prevAge > currentAge ? prev : current;
+    })
 };
 
 // Do not edit below this line
